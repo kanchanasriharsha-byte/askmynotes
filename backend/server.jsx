4749ab1462b890b03b5e432238d9c7183705955f
sri harshini <kanchanapallysriharshini6464@gmail.com>
2026-08-14 06:54:10 +0530

    Added root Docker files,README, and backend code

--- backend/server.jsx
+++ backend/server.jsx
@@ -1,13 +1,15 @@
 const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 5000;
+const cors = require('cors');
 
+const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('AskMyNotes API Running');
+  res.send('Backend is running');
 });
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
